17bc59ea7bcaa0ecb27557a1482a1346317ad534
VahanAper <asryanvahandev@gmail.com>
2017-05-24 11:41:08 +0400

    component and styles init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-title">Countdown to dec 25, 2017</div>
+        <div>
+          <div className="clock-days">14 days</div>
+          <div className="clock-hours">30 hours</div>
+          <div className="clock-minutes">15 minutes</div>
+          <div className="clock-seconds">20 seconds</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
       </div>
     );
   }
